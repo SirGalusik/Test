5ed264c590afbbb9b4691e4ec5bc2fdc0175171b
MrArtga <artga03@gmail.com>
2024-07-16 15:56:56 +0300

    Обработка формы

--- src/App.js
+++ src/App.js
@@ -1,29 +1,62 @@
 import { useState } from "react";
 
-import Content from "./Content";
-import RecButton from "./RecButton";
-
 function App(props) {
-  const [showMode, setShowNode] = useState(true);
+  // const [value, setValue] = useState('Text');
+  const [value, setValue] = useState('Яблоко');
+  const [checked, setChecked] = useState(false);
 
-  let content = null;
+  const items = ["Яблоко", "Банан", "Виноград"];
 
-  const handlerClick = () => {
-    setShowNode(!showMode);
-  }
+  const handlerChange = (e) => {
+    setValue(e.target.value)
 
-  if(showMode) {
-    content = <Content />;
+    console.log(e.target.value)
   }
 
   return (
     <div>
-      <RecButton />
+      {value}
+      <br />
+      <input 
+        value={value} 
+        onChange={(e) => setValue(e.target.value)}/>
 
-      <button onClick={handlerClick}>Click here</button>
-      {content}
+      <br />
+
+      <label>
+        Some state
+        <input 
+          type="checkbox" 
+          checked={checked} 
+          onChange={() => setChecked(!checked)}
+        />
+      </label>
+
+      <br />
+      
+      {/* <ul>
+        {items.map((item) => (
+          <li key={item}>
+            <label>
+              <input 
+                type="radio" 
+                checked={checked === item} 
+                onChange={() => setChecked(item)}
+              />
+                {item}
+            </label>
+          </li>
+        ))}
+      </ul> */}
+
+      <select value={value} onChange={handlerChange}>
+        <option value={"Яблоко"}>Яблоко</option>
+        <option value={"Банан"}>Банан</option>
+        <option value={"Виноград"}>Виноград</option>
+        <option value={"Арбуз"}>Арбуз</option>
+      </select>
+
     </div>
-
   )
 }
 
